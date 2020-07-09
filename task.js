7152494638cd3b04d1d5820b06dcc9a3fbf81d44
yeshwanthkonk <37962427+yeshwanthkonk@users.noreply.github.com>
2020-07-09 16:01:41 +0530

    Create task.js

---
+++ task.js
@@ -0,0 +1,17 @@
+//1.
+	let num = prompt("Enter Number")
+	let square = num ** 2
+//2.
+	let x = 1, y = 2
+	let temp = x
+	x = y
+	y = temp
+//3.
+	console.log(x+y+num)
+//4.
+	let celsius = 30
+	let fahrenheit = 9*(celsius/5) + 32
+//5.
+	for(let i = 1;i<=7;i++){
+		console.log('#'.repeat(i))
+	}
